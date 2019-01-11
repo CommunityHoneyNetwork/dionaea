7cbcdac6f604d0b2f378a0387be60beafcc1b911
amerck <alexander.t.merck@gmail.com>
2019-01-11 18:56:43 -0500

    Updating comments

--- clean_bistreams.sh
+++ clean_bistreams.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Compress bistream files older than 1 hour
+# Compress bistream files older than 5 minutes
 find /opt/dionaea/var/dionaea/bistreams/* -type f -mmin +5 -exec gzip {} \;
 
-# Clear bistream logs from dionaea every day
+# Clear bistream logs from dionaea every 60 minutes
 find /opt/dionaea/var/dionaea/bistreams/* -type f -mtime +60 -exec rm {} \;
 find /opt/dionaea/var/dionaea/bistreams/* -type d -empty -delete
