e38a7a09dd413db54ff6814839afcd08f04bded1
amerck <alexander.t.merck@gmail.com>
2019-01-11 19:41:40 -0500

    Modified find flags

--- clean_bistreams.sh
+++ clean_bistreams.sh
@@ -4,5 +4,5 @@
 find /opt/dionaea/var/dionaea/bistreams/* -type f -mmin +5 -exec gzip {} \;
 
 # Clear bistream logs from dionaea every 60 minutes
-find /opt/dionaea/var/dionaea/bistreams/* -type f -mtime +60 -exec rm {} \;
+find /opt/dionaea/var/dionaea/bistreams/* -type f -mmin +60 -exec rm {} \;
 find /opt/dionaea/var/dionaea/bistreams/* -type d -empty -delete
